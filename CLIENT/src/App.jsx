15369ab45c928e6faf52adb6c044c540bbe9abd4
Navin <rajnavinraj23@gmail.com>
2024-06-13 11:18:02 +0530

    Admin Page completed

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -28,6 +28,7 @@ import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { action as editJobAction } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
+import { loader as adminLoader } from "./pages/Admin";
 export const checkDeafultTheme = () => {
   const isDarkTheme = localStorage.getItem("dark-theme") === "true";
   document.body.classList.toggle("dark-theme", isDarkTheme);
@@ -85,6 +86,7 @@ const router = createBrowserRouter([
           {
             path: "admin",
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: "edit-job/:id",
